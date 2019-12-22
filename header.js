6aa2514d74ca976683f58d858b736120b8d56ff8
banya53github <59137987+banya53github@users.noreply.github.com>
2019-12-22 22:19:22 +0800

    Add files via upload

---
+++ header.js
@@ -0,0 +1,75 @@
+		header = '<div id="header">\
+						<h1><a href="index.html" id="logo"></a></h1>\
+							\
+						 <nav id="nav">\
+							<ul>\
+								\
+								<li><a href="index.html" alt="">回首頁</a></li>\
+                                <li><a href="about.html" alt="">關於忠孝</a></li>\
+                                <li><a href="http://school.kh.edu.tw/upload/314/104_47265/workplace.pdf">學校組織</a>\
+									<ul>\
+										<li><a href="http://school.kh.edu.tw/upload/314/104_47265/principal.pdf">校長室</a></li>\
+										<li><a href="http://school.kh.edu.tw/upload/314/104_47265/academic.pdf">教務處</a></li>\
+										<li><a href="http://affairs.kh.edu.tw/86">學務處</a></li>\
+										<li><a href="http://affairs.kh.edu.tw/267">總務處</a></li>\
+										<li><a href="http://school.kh.edu.tw/upload/314/104_47265/counsel.pdf">輔導處</a></li>\
+										<li><a href="http://school.kh.edu.tw/upload/314/104_47265/personnel.pdf">人事室</a></li>\
+										<li><a href="http://affairs.kh.edu.tw/3825">會計室</a></li>\
+										<li><a href="http://tw.pre.uschoolnet.com/school/chuhps-kh">幼兒園</a></li>\
+										<li><a href="https://www.facebook.com/%E9%B9%BD%E5%9F%95%E5%8D%80%E5%BF%A0%E5%AD%9D%E5%9C%8B%E5%B0%8F%E5%BF%97%E5%B7%A5%E5%9C%98-447167595715839/?tn-str=k*F">志工團</a></li>\
+									 </ul>\
+								</li>\
+                                <li><a href="#">課程發展</a>\
+									<ul>\
+										<li><a href="http://affairs.kh.edu.tw/3586">課程計畫</a></li>\
+										<li><a href="http://www.chuhps.kh.edu.tw/fprog/">特色課程</a></li>\
+										<li><a href="https://cirn.moe.edu.tw/">課程與教學資源(CIRN)</a></li>\
+									</ul>\
+								</li>\
+								<li><a href="#">校園服務</a>\
+									<ul>\
+										<li><a href="http://school.kh.edu.tw/upload/314/104_47265/calendar.pdf">行事曆</a></li>\
+										<li><a href="http://school.kh.edu.tw/upload/314/104_47265/%E9%AB%98%E9%9B%84%E5%B8%82%E9%B9%BD%E5%9F%95%E5%8D%80%E5%BF%A0%E5%AD%9D%E5%9C%8B%E5%B0%8F107%E5%AD%B8%E5%B9%B4%E5%BA%A6%E5%85%A8%E6%A0%A1%E4%B8%80%E9%80%B1%E4%BD%9C%E6%81%AF%E6%99%82%E9%96%93%E8%A1%A8.pdf">作息時間表</a></li>\
+										<li><a href="http://affairs.kh.edu.tw/3254">營養午餐</a></li>\
+										<li><a href="http://school.kh.edu.tw/upload/314/104_47265/%E7%84%A1%E9%9A%9C%E7%A4%99%E7%A9%BA%E9%96%93.pdf">無障礙空間</a></li>\
+										<li><a href="http://school.kh.edu.tw/upload/314/104_47265/%E9%98%B2%E7%81%BD%E5%9C%B0%E5%9C%96.pdf">防災地圖</a></li>\
+									</ul>\
+								</li>\
+                                <li><a href="#">行政E化</a>\
+									<ul>\
+										<li><a href="https://t5514887.tw2.quickconnect.to/">校園NAS</a></li>\
+										<li><a href="http://odm.kh.edu.tw/tbpg/">二代公文</a></li>\
+										<li><a href="http://epass.kh.edu.tw/passwd/index.jsp?chen=sas">校務系統</a></li>\
+										<li><a href="http://www4.inservice.edu.tw/">教師在職進修網</a></li>\
+										<li><a href="https://portal.kh.edu.tw/">資訊服務入口網</a></li>\
+										<li><a href="https://www.set.edu.tw/default.asp">特教通報網</a></li>\
+									</ul>\
+								</li>\
+                                <li><a href="#">學生連結</a>\
+									<ul>\
+										<li><a href="https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F%3Furp%3Dhttps%3A%2F%2Fmail.google.com%2Fa%2Fmail.chuhps.kh.edu.tw%26rli%3D74fafoztf3ri%26rld%3D1&service=mail&hd=mail.chuhps.kh.edu.tw&sacu=1&flowName=GlifWebSignIn&flowEntry=AddSession">Google登入</a></li>\
+										<li><a href="http://ireadinggames.kh.edu.tw/readerquiz/">喜閱網</a></li>\
+										<li><a href="http://netholiday.kh.edu.tw/web_index.action">網路假期</a></li>\										<li><a href="https://www.egame.kh.edu.tw/login">E-game</a></li>\
+										<li><a href="http://210.71.198.38/index_AIAL2.php?t=1567691914/">因材網</a></li>\
+										<li><a href="https://exam.tcte.edu.tw/tbt_html/">補救教學科技化評量</a></li>\
+										<li><a href="http://www.loxa.edu.tw/">Loxa教育網</a></li>\
+										<li><a href="https://isafeevent.moe.edu.tw/">全民資安素養</a></li>\
+										<li><a href="https://start.me/p/YakrnE">忠孝連結</a></li>\
+									</ul>\
+								</li>\
+                                \
+                               	<li><a href="#">主題網站</a>\
+									<ul>\
+										<li><a href="http://affairs.kh.edu.tw/2443">忠孝附幼本土語網</a></li>\
+										<li><a href="http://affairs.kh.edu.tw/1675">忠孝健康促進網</a></li>\
+										<li><a href="http://class.kh.edu.tw/9154">忠孝國小健康中心</a></li>\
+										<li><a href="http://affairs.kh.edu.tw/1284">忠孝國小本土教育網</a></li>\
+										<li><a href="http://affairs.kh.edu.tw/3627">忠孝國小資訊安全教育網</a></li>\
+										<li><a href="http://affairs.kh.edu.tw/3628">忠孝國小個資保護網</a></li>\
+									</ul>\
+								<li><a href="https://portal.kh.edu.tw/redirect.php?func=kh:school">登入</a>\
+								</li>\
+							</ul>\
+						</nav>\
+					</div>';
+						document.write(header);
